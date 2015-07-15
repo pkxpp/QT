2d60ddb4189e60b45f375f61311442e19f9cce86
page <pkxpp@126.com>
2015-07-16 00:19:46 +0800

    html style. I think it's qml language

--- C++ GUI Qt4/hello/hello.cpp
+++ C++ GUI Qt4/hello/hello.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-	QLabel *label = new QLabel("Hello Qt!");
+    QLabel *label = new QLabel("<h2><i>Hello</i>"
+                               "<font color=red>QT!</font></h2>");
 	label->show();
 	return app.exec();
 }
